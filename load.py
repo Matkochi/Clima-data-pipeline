870e19adfe1087fb50d0df3affdde59fb7edcbec
Mateus Takashi Kochi <89741576+Matkochi@users.noreply.github.com>
2025-05-10 19:58:07 -0300

    Delete load.py

--- load.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-import os
-
-def save_to_db(df):
-    db_path = os.getenv("DB_PATH", "data/weather.db")
-    conn = sqlite3.connect(db_path)
-    df.to_sql('weather', conn, if_exists='append', index=False)
-    conn.close()
